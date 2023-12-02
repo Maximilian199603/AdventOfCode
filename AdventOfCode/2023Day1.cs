73a15832a68c417556807fe8397ebc398b93ae4c
Maximilian1996 <magnitudetwende@gmail.com>
2023-12-02 21:44:27 +0100

    Add project files.

---
+++ AdventOfCode/2023Day1.cs
@@ -0,0 +1,82 @@
+﻿using Ornaments.Solutions;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+[RegisterOrnament("Trebuchet?!", 2023, 1)]
+internal  sealed partial class _2023Day1 : ISolution
+{
+    private static Dictionary<string, int> _lookup = new Dictionary<string, int>
+    {
+        { "zero" , 0 },
+        { "one"  , 1 },
+        { "two"  , 2 },
+        { "three", 3 },
+        { "four" , 4 },
+        { "five" , 5 },
+        { "six"  , 6 },
+        { "seven", 7 },
+        { "eight", 8 },
+        { "nine" , 9 },
+        { "0"    , 0 },
+        { "1"    , 1 },
+        { "2"    , 2 },
+        { "3"    , 3 },
+        { "4"    , 4 },
+        { "5"    , 5 },
+        { "6"    , 6 },
+        { "7"    , 7 },
+        { "8"    , 8 },
+        { "9"    , 9 },
+    };
+
+    [GeneratedRegex("[0-9]|one|two|three|four|five|six|seven|eight|nine", RegexOptions.Compiled)]
+    private static partial Regex ExtractNumbers();
+
+    [GeneratedRegex("[0-9]|one|two|three|four|five|six|seven|eight|nine", RegexOptions.Compiled | RegexOptions.RightToLeft)]
+    private static partial Regex ExtractNumbersRtl();
+
+    //TODO: Method does not work properly in case of eightwothree it returns only 83 not 823
+    public static int ExtractCalibrationValue(string s, bool partTwo)
+    {
+        var matchesLeft = ExtractNumbers().Matches(s);
+        var matchesRight = ExtractNumbersRtl().Matches(s);
+        int result = 0;
+
+        if (matchesLeft.Count > 0)
+        {
+            var a = _lookup[matchesLeft.First(x => partTwo || int.TryParse(x.Value, out var _)).Value];
+            var b = _lookup[matchesRight.First(x => partTwo || int.TryParse(x.Value, out var _)).Value];
+            result = a * 10 + b;
+        }
+        return result;
+    }
+
+
+
+    public async Task<object> DoPartOneAsync(ISolutionContext solutionContext)
+    {
+        await Task.CompletedTask;
+        var input = solutionContext.As<IEnumerable<string>>();
+        return input
+            .Select(s => ExtractCalibrationValue(s, false))
+            .Sum();
+    }
+
+    public async Task<object> DoPartTwoAsync(ISolutionContext solutionContext)
+    {
+        await Task.CompletedTask;
+        var input = solutionContext.As<IEnumerable<string>>();
+        return input
+            .Select(s => ExtractCalibrationValue(s, true))
+            .Sum();
+    }
+
+    public bool TryParse(string input, out object parsed)
+    {
+        parsed = input.Trim()
+            .ReplaceLineEndings()
+            .Split($"{Environment.NewLine}");
+
+        return true;
+    }
+}
